26032cc78c8ae20096ff32af1c22694cf85ff5b9
akram-abid <a.abid44778@gmail.com>
2025-05-27 20:39:26 +0100

    complete the projetc

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require("node:path");
 
 const messages = [
   {
+    id: 0,
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    id: 1,
     text: "Hello World!",
     user: "Charles",
     added: new Date()
@@ -16,17 +18,40 @@ const messages = [
 ];
 
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.render("index", { messagesDB: messages })
-})
+    res.render("index", { title: "Mini Messageboard", messagesDB: messages })
+});
 
 app.get("/new", (req, res) => {
-    res.render("index", {message: "fuck this"})
-})
+    res.render("form", {})
+});
+
+app.post("/new", (req, res) => {
+  messages.push({
+    id: messages.length,
+    user: req.body.user,
+    text: req.body.text,
+    added: new Date()
+  });
+  res.redirect("/");
+});
+
+app.get("/message/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages.find((msg) => msg.id === messageId);
+
+  if (message) {
+    res.render("message", { message });
+  } else {
+    res.status(404).send("Message not found");
+  }
+});
 
 app.listen(3000,  () => {
   console.log(`My first Express app - listening on port 3000!`);
