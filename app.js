f993fb81a5cc6df069f79541a3011589bae54dd9
akram-abid <a.abid44778@gmail.com>
2025-05-28 13:02:46 +0100

    preparing for deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("node:path");
+const port = process.env.PORT || 3000
 
 const messages = [
   {
@@ -53,6 +54,6 @@ app.get("/message/:id", (req, res) => {
   }
 });
 
-app.listen(3000,  () => {
-  console.log(`My first Express app - listening on port 3000!`);
+app.listen(port,  () => {
+  console.log(`My first Express app - listening on port `, port);
 });
